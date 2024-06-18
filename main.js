16a208cb799ab1888b1de18643773430ed1c3045
Mark Robson <m.robson@nchain.com>
2024-06-18 15:08:08 +0100

    refactor: adding screens

--- main.js
+++ main.js
@@ -5,28 +5,19 @@ import {
   percentageCorrect,
 } from "./index.js";
 
+const roundDurationMs = 3000;
 let n = 2;
-let roundDurationMs = 3000;
-let game = newGame({
-  n,
-  gameDurationMs: 60000,
-  counter: 0,
-  history: [],
-  userInputHistory: [],
-});
-let numberOfRounds = Math.floor(game.gameDurationMs / roundDurationMs);
-game.history = [];
-for (let i = 0; i <= numberOfRounds; i++) {
-  game.history.push(generateRound());
-}
+let playerPositionResponse;
+let playerLetterResponse;
+let game;
 
-document.getElementById("n").textContent = n;
+document.getElementById("start-button").addEventListener("click", startGame);
+document
+  .getElementById("play-again-button")
+  .addEventListener("click", showIntroScreen);
 
 const soundButton = document.getElementById("soundButton");
 const positionButton = document.getElementById("positionButton");
-
-let playerPositionResponse;
-let playerLetterResponse;
 
 soundButton.addEventListener("click", () => {
   playerLetterResponse = game.history[game.counter - n].letter;
@@ -35,10 +26,33 @@ positionButton.addEventListener("click", () => {
   playerPositionResponse = game.history[game.counter - n].position;
 });
 
+function showIntroScreen() {
+  document.getElementById("intro-screen").classList.remove("hidden");
+  document.getElementById("game-screen").classList.add("hidden");
+  document.getElementById("overview-screen").classList.add("hidden");
+}
+
+function showGameScreen() {
+  document.getElementById("intro-screen").classList.add("hidden");
+  document.getElementById("game-screen").classList.remove("hidden");
+  document.getElementById("overview-screen").classList.add("hidden");
+}
+
+function showOverviewScreen() {
+  document.getElementById("intro-screen").classList.add("hidden");
+  document.getElementById("game-screen").classList.add("hidden");
+  document.getElementById("overview-screen").classList.remove("hidden");
+}
+
 function displayCell(position) {
   const cells = document.querySelectorAll(".cell");
   cells.forEach((cell) => cell.classList.remove("highlight"));
   document.getElementById("cell" + position).classList.add("highlight");
+}
+
+function clearCells() {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => cell.classList.remove("highlight"));
 }
 
 function playSound(letter) {
@@ -48,7 +62,26 @@ function playSound(letter) {
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-(async () => {
+async function startGame() {
+  n = parseInt(document.getElementById("n").value);
+  console.log(n);
+  const gameDurationMs =
+    parseInt(document.getElementById("duration").value) * 1000;
+  document.getElementById("n-counter").textContent = n;
+  game = newGame({
+    n,
+    gameDurationMs,
+    counter: 0,
+    history: [],
+    userInputHistory: [],
+  });
+  let numberOfRounds = Math.floor(game.gameDurationMs / roundDurationMs);
+  game.history = [];
+  for (let i = 0; i <= numberOfRounds; i++) {
+    game.history.push(generateRound());
+  }
+  clearCells();
+  showGameScreen();
   await delay(roundDurationMs);
   // Start the game
   for (game.counter = 0; game.counter < game.history.length; game.counter++) {
@@ -87,6 +120,12 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
   });
 
   game.stats = percentageCorrect(game.results);
-  console.log("Game over");
-  console.log(game);
-})();
+  showOverviewScreen();
+  document.getElementById("results").textContent = JSON.stringify(
+    game.stats,
+    null,
+    2
+  );
+}
+
+showIntroScreen();
